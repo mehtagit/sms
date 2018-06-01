10f39967f2aeafdc05b4d23851facc84049b7811
mehtagit <33978751+mehtagit@users.noreply.github.com>
2018-06-01 11:16:40 +0530

    SMS Filter

--- SmsFilter/src/main/java/com/sms/service/DeliveryService.java
+++ SmsFilter/src/main/java/com/sms/service/DeliveryService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sms.bean.Delivery;
-import com.sms.controller.RequestController;
 import com.sms.repository.RequestRepository;
 import com.sms.util.Utility;
 
